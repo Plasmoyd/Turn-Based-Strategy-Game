2468f3f462c5bc7eb56abea7e823d8b64c3ec78b
Ilija <ilijastarcevicgamedev@gmail.com>
2022-08-09 16:40:50 +0200

    Imported animations. Implemented idle and run animations and transitions between them.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,17 +6,26 @@ public class Unit : MonoBehaviour
 {
 
     [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float rotationSpeed = 4f;
     [SerializeField] float stoppingDistance = .1f;
+    [SerializeField] Animator unitAnimator;
 
     private Vector3 targetPosition;
 
     void Update()
     {
-        Vector3 moveDirection = (targetPosition - transform.position).normalized; //without normalizing, we would have a direction vector with a magnitude applied
 
-        if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized; //without normalizing, we would have a direction vector with a magnitude applied
             transform.position += movementSpeed * Time.deltaTime * moveDirection;
+
+            unitAnimator.SetBool("isRunning", true);
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+        }
+        else
+        {
+            unitAnimator.SetBool("isRunning", false);
         }
         
 
